44ee7fadf0ffe73989b9dc2cb9aa6fd33f9ba879
tonybonaducci <drlmenezes21@gmail.com>
2024-10-25 21:29:28 +0100

    hohohihi

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,12 +4,29 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font = wezterm.font_with_fallback {
+	'Iosevka',
+	'Symbols Nerd Font Mono',
+}
+config.font_rules =
+{
+	{
+		italic = true,
+		intensity = "Normal",
+		font = wezterm.font_with_fallback
+		{
+			'Iosevka',
+			'Symbols Nerd Font Mono',
+		},
+	},
+}
+
+config.window_background_image = "./1-filter.jpg"
 config.font_size = 18
 
 config.enable_tab_bar = false
 config.window_decorations = "TITLE | RESIZE"
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.8
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
